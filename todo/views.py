cda3b690175ddd7be047d21f21386f8f0b8f1f7b
Francois Polo <andasan@gmail.com>
2021-02-26 10:38:31 -0800

    try-except

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -9,7 +10,20 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', { 'form': UserCreationForm() })
     else:
         if request.POST['password1'] == request.POST['password2']:
-            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(
+                    request,
+                    'todo/signupuser.html',
+                    {
+                        'form': UserCreationForm(),
+                        'error':'That username has already been taken. Please choose a new name'
+                    } 
+                )
+            
+
         else:
             #Tell the user that the password do not match
             return render(request, 'todo/signupuser.html', { 'form': UserCreationForm(), 'error':'Passwords did not match' })
