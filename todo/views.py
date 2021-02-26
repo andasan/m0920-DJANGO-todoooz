a79eebae6dab2ac88af687227842087498cb90da
Francois Polo <andasan@gmail.com>
2021-02-26 10:26:00 -0800

    1st error handling

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', { 'form': UserCreationForm() })
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', { 'form': UserCreationForm() })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+        else:
+            #Tell the user that the password do not match
+            return render(request, 'todo/signupuser.html', { 'form': UserCreationForm(), 'error':'Passwords did not match' })
